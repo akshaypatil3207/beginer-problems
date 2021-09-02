b98a08f83a52941786dad0bf33e018fd2ae0d35b
akshaypatil3207 <80540192+akshaypatil3207@users.noreply.github.com>
2021-09-02 13:03:02 +0530

    Update longest unique substring length.py

--- longest unique substring length.py
+++ longest unique substring length.py
@@ -1,28 +1,29 @@
 out=[]
 prev_index=0
-#a=0
-word=input()
-done=" "
-inde=[]
+word=input("Enter the word:- ").strip()                 #taking long string from user
+sub_string=" "
+imp_index=[]
 j=0
-while True:
-    if word[j] in done:
-        #a=j-prev_index
-        out.append(len(done)-1)
-        print(done)
-        prev_index=inde[done.index(word[j])]
+print("Sub strings are: -")
+while True:                                              #iterating through string
+    if word[j] in sub_string:                            #if letter in substring, then note len of substring 
+        out.append(len(sub_string)-1)
+        print(sub_string)
+        prev_index=imp_index[sub_string.index(word[j])]  #start again from the letter next to previous apperence of the repeted letter.
         j=prev_index-1
-        done=" "
-        inde=[] 
-    else:
-        done=done+word[j]
-        inde.append(j)
+        sub_string=" "
+        imp_index=[] 
+    else:                                                 #if letter not in substring then add it to substring 
+        sub_string=sub_string+word[j]
+        imp_index.append(j)
         
     j+=1
     if j==(len(word)-1):
-        out.append(len(done))
+        out.append(len(sub_string))
         break
 out.sort()    
-print(out[len(out)-1])
+print("Length of longest unique substring is :- {} ".format(out[len(out)-1]))
+
+
 
 
