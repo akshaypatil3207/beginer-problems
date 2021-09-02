6ed550dcfff03596b18bbf97b73b41b1cba785d2
akshaypatil3207 <80540192+akshaypatil3207@users.noreply.github.com>
2021-09-02 13:12:37 +0530

    Update which tank will empty first.py

--- which tank will empty first.py
+++ which tank will empty first.py
@@ -1,24 +1,29 @@
-a=int(input()) # no of tanks
+a=int(input("Enter the no of tanks:- ")) # no of tanks
 tank_level=[]
 leak_rate=[]
 out=[]
 for i in range(a):
-    tank_level.append(int(input()))
-    leak_rate.append(int(input()))
-    out.append(tank_level[i]/leak_rate[i])
+    tank_level.append(int(input("Enter tank level of no {} tank :- ".format(i))))
+    leak_rate.append(int(input("Enter leak rate of no {} tank:- ".format(i))))
+    out.append(tank_level[i]/leak_rate[i])                                        #calculating deciding factor
 for i in range(a):
     b=0
-    for j in range(a):
+    for j in range(a):                                                             #finding leat deciding factor
         if out[i]<=out[j] and j!=i:
             b=b+1
         if b==(a-1):
             least=out[i]
             break
 final=[]
-for i in range(a):
-    if out[i]==least:
+for i in range(a):                             #printing tank number which will empty first, 
+    if out[i]==least:                          #if two tanks are empting together then print both tank nos in ascending order seperated by comma
         final.append(i)
+print("These tank will empty first/together")
 print(*final, sep = ", ") 
+    
+    
+    
+
     
     
     
